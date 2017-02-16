44f144450f7b863beb60b8c3c8d39978bf4278c7
Deepak Kumar <deepak@whyable.com>
2017-02-16 18:49:31 -0500

    W3C - warning fix for viewport scaling

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php require 'content.php';?>
 <!DOCTYPE html>
 <head>
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- Latest compiled and minified bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
